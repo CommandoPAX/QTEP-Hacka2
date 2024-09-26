28806541cee0e71e3618b83c33682a75792676e2
ACGuerrero <acastillo@ciencias.unam.mx>
2024-09-26 12:51:58 +0200

    my code is like an umbrella in the sahara

---
+++ vectorization.py
@@ -0,0 +1,50 @@
+import numpy as np
+import scipy.integrate as spi
+
+def ket_to_density(ket) :
+    return np.outer(ket, ket.conj())
+
+def rho_derivative(rho, H, L) :
+    # Takes as arguments the Hamiltonian H and the list of jump operators L
+
+    # Compute Hamiltonian part :
+    hamiltonian_part = -1j*(np.dot(H,rho) - np.dot(rho,H))
+    linblad_part = 0
+
+    # Compute non-Hamiltonian part :
+    for jump_operator in L:
+        linblad_part += np.dot(np.dot(jump_operator,rho),jump_operator.conj().T) - 0.5*np.dot(jump_operator.conj().T,jump_operator,rho) - 0.5*np.dot(rho,jump_operator.conj().T,jump_operator)
+
+    # Return the sum of the two parts :
+    return hamiltonian_part + linblad_part
+
+def solver(rho,f) :
+    rho_size = rho.size
+
+    vectorized_rho = rho.reshape(rho_size,1)
+
+    result = spi.RK45(f,0,vectorized_rho,1)
+    
+    return result
+
+def evolution(vec,f,rho_dims) :
+    # Shape into matrix
+    rho = vec.reshape(rho_dims)
+    # Evolve using master equation
+    rho = f(rho)
+    # Reshape into vector
+    rho = rho.reshape(rho.size,1)
+    return rho
+
+# Initial state
+ket_init = np.array([1,1])/np.sqrt(2)
+rho_init = np.outer(ket_init,ket_init.conj())
+
+# Evolution
+Hamiltonian = np.array([[1,0],[0,-1]])
+Jump_list = []
+
+f = lambda rho : rho_derivative(rho,Hamiltonian,Jump_list)
+
+result = solver(rho_init,f)
+print(type(result))
